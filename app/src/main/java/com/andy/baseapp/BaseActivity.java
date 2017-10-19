5db3aa8be896572a4215f90f3516cc4e96b4baf6
李小宁 <lixn@fxiaoke.com>
2017-10-19 14:45:33 +0800

    BaseActivity添加过滤tag

--- app/src/main/java/com/andy/baseapp/BaseActivity.java
+++ app/src/main/java/com/andy/baseapp/BaseActivity.java
@@ -3,7 +3,10 @@
  */
 package com.andy.baseapp;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by lixn on 2017/10/13.
@@ -11,4 +14,11 @@ import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
 
+    private static final String TAG_ACT = "act";   // 可以通过过滤这个tag快速找到当前的Activity的名字
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG_ACT, "[act:] "+getClass().getName());
+    }
 }
