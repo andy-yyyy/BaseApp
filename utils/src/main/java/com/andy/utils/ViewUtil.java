59dfcc2e482e75055022fc273fbc17046c903162
李小宁 <lixn@fxiaoke.com>
2017-11-03 17:44:36 +0800

    ViewUtil增加列表平滑滚动到指定位置的方法

--- utils/src/main/java/com/andy/utils/ViewUtil.java
+++ utils/src/main/java/com/andy/utils/ViewUtil.java
@@ -4,14 +4,76 @@
 package com.andy.utils;
 
 import android.graphics.Rect;
+import android.os.Handler;
+import android.util.Log;
 import android.view.TouchDelegate;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 
 /**
  * Created by lixn on 2017/6/13.
  */
 
 public class ViewUtil {
+
+    /**
+     * 将一个AbsListView平滑滚动到指定位置（修复系统AbsListView#smoothScrollToPositionFromTop()方法的bug）
+     * @param view
+     * @param position
+     */
+    public static void smoothScrollToPositionFromTop(final AbsListView view, final int position) {
+        View child = getChildAtPosition(view, position);
+        // // There's no need to scroll if child is already at top or view is already scrolled to its end
+        if (child != null && ((child.getTop() == 0))) {
+            return;
+        }
+        view.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(final AbsListView view, final int scrollState) {
+                if (scrollState == SCROLL_STATE_IDLE) {
+                    view.setOnScrollListener(null);
+                    // Fix for scrolling bug
+                    new Handler().post(new Runnable() {
+                        @Override
+                        public void run() {
+                            view.setSelection(position);
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+            }
+        });
+        // Perform scrolling to position
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                view.smoothScrollToPositionFromTop(position, 0);
+            }
+        });
+    }
+
+    /**
+     * 获取ListView等适配器视图中某个位置的View
+     * @param view
+     * @param position
+     * @return
+     */
+    public static View getChildAtPosition(final AdapterView view, final int position) {
+        if (view == null) {
+            return null;
+        }
+        final int index = position - view.getFirstVisiblePosition();
+        if (index >= 0 && index < view.getChildCount()) {
+            return view.getChildAt(index);
+        } else {
+            return null;
+        }
+    }
 
     /**
      * 设置View的触摸事件代理（如扩大按钮的点击范围）
